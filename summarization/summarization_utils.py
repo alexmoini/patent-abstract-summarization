c28e3d4dff4fc1e3da34ff7e6c87efce075f3f12
Alexander Tyler Moini <92174565+alexmoini@users.noreply.github.com>
2022-12-07 17:21:35 -0700

    utils

---
+++ summarization/summarization_utils.py
@@ -0,0 +1,39 @@
+import torch
+import transformers
+import pandas as pd
+import datasets
+
+class SummarizationDataModule(torch.utils.data.Dataset):
+    """
+    
+    """
+    def __init__(self, dataset_path, tokenizer, max_length=1024):
+        """
+        
+        """
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        # load big_patent_data
+        self.data = pd.read_csv(dataset_path)
+        self.text = self.data['description']
+        self.labels = self.data['abstract']
+
+    def __getitem__(self, index):
+        """
+        
+        """
+        text = self.text[index]
+        label = self.labels[index]
+        encoding = self.tokenizer(text, truncation=True, padding='max_length',
+                                  max_length=self.max_length, return_tensors='pt')
+        encoding['labels'] = self.tokenizer(label, truncation=True, padding='max_length',
+                                            max_length=self.max_length, return_tensors='pt')['input_ids']
+        
+        for key in encoding.keys():
+            encoding[key] = encoding[key].squeeze(0)
+        return encoding
+    def __len__(self):
+        """
+        
+        """
+        return len(self.text)
