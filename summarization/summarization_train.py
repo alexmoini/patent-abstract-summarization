068ffaa5b99b68820f211cb172cb03aa468f55b0
jeha7904 <112502595+jeha7904@users.noreply.github.com>
2022-12-07 18:35:20 -0700

    Update summarization_train.py
    '

--- summarization/summarization_train.py
+++ summarization/summarization_train.py
@@ -18,12 +18,20 @@ logger = logging.get_logger("transformers")
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model-architecture", type=str, default="facebook/bart-large")
+    parser.add_argument("--model-architecture", type=str, default=\
+                        "facebook/bart-large")
     parser.add_argument("--model-dir", type=str, default=None)
-    parser.add_argument("--train", type=str, default='s3://deeplearning-nlp-bucket/summary_patent_data/train')
-    parser.add_argument("--eval", type=str, default='s3://deeplearning-nlp-bucket/summary_patent_data/validation')
-    parser.add_argument("--output-dir", type=str, default='/Users/alexandermoini/SWE4S-DeepLearning-NLP-Project/summarization/models/bart-base')
-    parser.add_argument("--logs-dir", type=str, default='/Users/alexandermoini/SWE4S-DeepLearning-NLP-Project/summarization/logs/bart-base')
+    parser.add_argument("--train", type=str, default='s3://deeplearning-\
+                        nlp-bucket/summary_patent_data/train')
+    parser.add_argument("--eval", type=str, default=\
+                        's3://deeplearning-nlp-bucket/\
+                        summary_patent_data/validation')
+    parser.add_argument("--output-dir", type=str, default=\
+                        '/Users/alexandermoini/SWE4S-DeepLearning-\
+                        NLP-Project/summarization/models/bart-base')
+    parser.add_argument("--logs-dir", type=str, default=\
+                        '/Users/alexandermoini/SWE4S-DeepLearning-NLP-Project\
+                        /summarization/logs/bart-base')
     parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument("--num-epochs", type=int, default=1)
     parser.add_argument("--learning-rate", type=float, default=1e-3)
@@ -35,11 +43,20 @@ def get_args():
     args, _ = parser.parse_known_args()
     return args
 
-def load_dataloaders(dataset_path_train, dataset_path_eval, tokenizer, sequence_length, batch_size, num_workers):
-    train_dataset = SummarizationDataModule(dataset_path_train, tokenizer, max_length=sequence_length)
-    val_dataset = SummarizationDataModule(dataset_path_eval, tokenizer, max_length=sequence_length)
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+def load_dataloaders(dataset_path_train, dataset_path_eval, tokenizer, \
+                     sequence_length, batch_size, num_workers):
+    train_dataset = SummarizationDataModule(dataset_path_train, tokenizer, \
+                                            max_length=sequence_length)
+    val_dataset = SummarizationDataModule(dataset_path_eval, \
+                                          tokenizer, \
+                                          max_length=sequence_length)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset\
+                                                   , batch_size=batch_size, \
+                                                   num_workers=num_workers, \
+                                                   shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, \
+                                                 num_workers=num_workers, \
+                                                 shuffle=False)
     return train_dataloader, val_dataloader
 
 def load_summarization_model(model_architecture, checkpoint_dir, learning_rate):
@@ -56,9 +73,12 @@ def train(args):
     logger.info("Loading tokenizer")
     tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_architecture)
     logger.info("Loading dataloaders")
-    train_dataloader, val_dataloader = load_dataloaders(args.train, args.eval, tokenizer, args.max_seq_length, args.batch_size, args.num_workers)
+    train_dataloader, val_dataloader = load_dataloaders(args.train, args.eval, \
+                                                        tokenizer, args.max_seq_length, \
+                                                        args.batch_size, args.num_workers)
     logger.info("Loading model")
-    model = load_summarization_model(args.model_architecture, args.model_dir, args.learning_rate)
+    model = load_summarization_model(args.model_architecture, args.model_dir, \
+                                     args.learning_rate)
     logger.info("Creating callbacks")
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
@@ -75,7 +95,8 @@ def train(args):
         callbacks=[checkpoint_callback, lr_monitor],
         accelerator='cpu',
         precision=16,
-        logger=DAGsHubLogger(metrics_path=args.logs_dir+'metrics.csv', hparams_path=args.logs_dir+'hparams.csv'),
+        logger=DAGsHubLogger(metrics_path=args.logs_dir+'metrics.csv',\
+                             hparams_path=args.logs_dir+'hparams.csv'),
     )
     logger.info("Starting training")
     trainer.fit(model, train_dataloader, val_dataloader)
@@ -86,3 +107,4 @@ def train(args):
 if __name__ == "__main__":
     args = get_args()
     train(args)
+    
