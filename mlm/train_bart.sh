12520db23d6491220df8b602bc1963cac3a6caf4
Alexander Tyler Moini <92174565+alexmoini@users.noreply.github.com>
2022-12-07 18:01:17 -0700

    training example

---
+++ mlm/train_bart.sh
@@ -0,0 +1,1 @@
+python mlm_train.py --model-architecture facebook/bart-large --max-seq-length 2048 --output-dir models/bart-large/ --logs-dir logs/bart-large/ --hardware mps --gpus 1 --num-epochs 1 --batch-size 16 --learning-rate 2e-3 --masked-lm-prob 0.20 --hardware mps
