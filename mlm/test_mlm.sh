4973905b5b4cce2a397cfcf98afe7cf23202c09c
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-12-12 07:43:48 -0700

    Add mlm functional test

--- mlm/test_mlm.sh
+++ mlm/test_mlm.sh
@@ -1,3 +1,7 @@
-test -e ssshtest || wget -qhttps://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+# Need to fix S3 permissions
+run train_mlm python mlm_train.py --model-architecture facebook/bart-large --max-seq-length 2048 --output-dir models/bart-large/ --logs-dir logs/bart-large/ --hardware cpu --gpus 0 --num-epochs 1 --batch-size 16 --learning-rate 2e-3 --masked-lm-prob 0.20
+
+assert_exit_code 0
