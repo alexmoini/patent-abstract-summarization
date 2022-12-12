fa7c6558a67bcc5c51fa348df549cff741f5ed8c
William J. Johnson <85764770+williamjjohnson@users.noreply.github.com>
2022-12-12 07:35:06 -0700

    Fix pep8 embedding_backbone.py

--- mlm/embedding_backbone.py
+++ mlm/embedding_backbone.py
@@ -6,7 +6,10 @@ class TextEmbeddingBackbone(torch.nn.Module):
     """
     Embedding backbone for down stream tasks
     """
-    def __init__(self, model_architecture, checkpoint_dir=None, freeze=False, pool_outputs=False):
+    def __init__(
+            self, model_architecture,
+            checkpoint_dir=None, freeze=False,
+            pool_outputs=False):
         """
         Model name can be any of the models from the transformers library
         Freeze is a boolean to freeze the weights of the model
@@ -19,8 +22,10 @@ class TextEmbeddingBackbone(torch.nn.Module):
             checkpoint = torch.load(checkpoint_dir)
             for key in list(checkpoint['state_dict'].keys()):
                 new_key = key.replace('model.', '')
-                checkpoint['state_dict'][new_key] = checkpoint['state_dict'].pop(key)
-            self.model.load_state_dict(checkpoint['state_dict'], strict=False) #hide unexpected keys bias,fc.weight,fc.bias 
+                checkpoint['state_dict'][new_key] = \
+                    checkpoint['state_dict'].pop(key)
+            # hide unexpected keys bias,fc.weight,fc.bias
+            self.model.load_state_dict(checkpoint['state_dict'], strict=False)
         if freeze:
             for param in self.model.parameters():
                 param.requires_grad = False
@@ -32,7 +37,8 @@ class TextEmbeddingBackbone(torch.nn.Module):
         """
         Forward pass of the model
         """
-        model_output = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        model_output = self.model(
+                input_ids=input_ids, attention_mask=attention_mask)
         if self.pool_outputs:
             return self.__mean_pooling(model_output, attention_mask)
         else:
@@ -40,12 +46,18 @@ class TextEmbeddingBackbone(torch.nn.Module):
 
     def __mean_pooling(self, model_output, attention_mask):
         """
-        mean pooling of the model output, pools along the sequence dimension, all the padded tokens are ignored
+        mean pooling of the model output
+        pools along the sequence dimension
+        all the padded tokens are ignored
         """
-        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-    
+        # First element of model_output contains all token embeddings
+        token_embeddings = model_output[0]
+        input_mask_expanded = attention_mask.unsqueeze(
+                -1).expand(token_embeddings.size()).float()
+        return torch.sum(
+                token_embeddings * input_mask_expanded,
+                1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
     def __len__(self):
         """
         Returns length of parameters
